cc5072c1ed2397601e89a6a5955284cb49e57695
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-02 16:32:14 +0530

    uc2

--- Program.cs
+++ Program.cs
@@ -11,17 +11,26 @@ namespace EmployeeDetails
         public static void Main()
         {
             int Is_Full_Time = 1;
+            int If_Part_Time = 2;
+            int Wage_per_Hour = 20;
+            int Total_Employee_Wage;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             if (empCheck == Is_Full_Time)
             {
-                Console.WriteLine("Present");
+                Total_Employee_Wage = 8 * Wage_per_Hour;
+                Console.WriteLine(Total_Employee_Wage);
+            }
+            else if (empCheck == If_Part_Time)
+            {
+                Total_Employee_Wage = 4 * Wage_per_Hour;
+                Console.WriteLine(Total_Employee_Wage);
             }
             else
             {
-                Console.WriteLine("Absent");
+                Console.WriteLine("Employee is Absent");
             }
         }
 
