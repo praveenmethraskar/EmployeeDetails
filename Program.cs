99ca23796fc9338b9925c5fd8b34de561ae488ec
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-02 16:14:06 +0530

    Uc 1 problem

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeDetails
+{
+    public class program
+    {
+
+
+
+
+        public static void Main()
+        {
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+
+
+    }
+}
